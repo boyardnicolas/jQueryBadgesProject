1d6add4fb8813920e4840c70c0601b9657738c3b
nicolas <boyard.nicolas@gmail.com>
2018-02-13 05:29:47 +0100

    displaying completed courses

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,16 @@
 $(function() {
 
   // your code will go here
-
+  $.ajax({
+    url: 'https://www.codeschool.com/users/boboy.json',
+    dataType: 'jsonp',
+    success: function(response) {
+		var elt;
+      for(i in response.courses.completed) {
+		elt = response.courses.completed[i];
+		$('<div>').addClass('course').appendTo($('#badges')).append('<h3>'+elt.title+'</h3><img src="'+elt.badge+'" /><a href="'+elt.url+'" target="_blank" class="btn btn-primary">See Course</a>');
+	  }
+    }
+  });
+	
 });
